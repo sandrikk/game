d93722f2c7219bd0ab08af52bafd30f98c8b5331
Sandrikk <69211090+sandrikk@users.noreply.github.com>
2022-11-29 12:32:33 +0100

    add game window feature

--- src/main/Game.java
+++ src/main/Game.java
@@ -1,7 +1,9 @@
 package main;
 
 public class Game {
+    private GameWindow gameWindow;
     public Game() {
-        System.out.println("I am alive");
+
+        gameWindow = new GameWindow();
     }
 }
--- src/main/GameWindow.java
+++ src/main/GameWindow.java
@@ -1,4 +1,12 @@
 package main;
 
+import javax.swing.*;
+
 public class GameWindow {
+    private JFrame jframe;
+    public GameWindow() {
+        jframe = new JFrame();
+        jframe.setSize(400,400);
+        jframe.setVisible(true);
+    }
 }
