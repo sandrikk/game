9d96c00b363a5c0ffbc0e0b4a7af146325f66adc
Sandrikk <69211090+sandrikk@users.noreply.github.com>
2022-12-06 12:37:53 +0100

    add gamePanel

--- src/main/Game.java
+++ src/main/Game.java
@@ -2,9 +2,10 @@ package main;
 
 public class Game {
     private GameWindow gameWindow;
+    private GamePanel gamePanel;
     public Game() {
+        gamePanel = new GamePanel();
+        gameWindow = new GameWindow(gamePanel);
 
-        gameWindow = new GameWindow();
-        //blablabla
     }
 }
