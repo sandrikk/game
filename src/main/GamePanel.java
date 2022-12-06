c4b9380532d43cc1a3b2e36ab4ef6d8ec3ca7c00
Sandrikk <69211090+sandrikk@users.noreply.github.com>
2022-12-06 17:47:20 +0100

    adding keyboards and mouse inputs
    adding features to move the rectangle

--- src/main/GamePanel.java
+++ src/main/GamePanel.java
@@ -1,16 +1,37 @@
 package main;
 
+import inputs.Keyboardinputs;
+import inputs.Mouseinputs;
+
 import java.awt.*;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 import javax.swing.JPanel;
 
 public class GamePanel extends JPanel {
+    private Mouseinputs mouseInputs;
+    private int xDelta = 0, yDelta = 0;
     public GamePanel() {
+        mouseInputs = new Mouseinputs();
+        addKeyListener(new Keyboardinputs(this));
+        addMouseListener(mouseInputs);
+        addMouseMotionListener(mouseInputs);
+    }
 
+    public void changeXDelta(int value) {
+        this.xDelta += value;
+        repaint();
+    }
+
+    public void changeYDelta(int value) {
+        this.yDelta += value;
+        repaint();
     }
 
     public void paintComponent(Graphics g) {
         super.paintComponent(g);
 
-        g.fillRect(50,50,50,100);
+        g.fillRect(100+xDelta,100+yDelta,50,100);
+
     }
 }
