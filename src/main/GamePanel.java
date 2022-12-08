a56e7870a641b8c53be2ee578d8a56fbb587206c
georgistoychev003 <118820392+georgistoychev003@users.noreply.github.com>
2022-12-08 02:57:50 +0100

    simple game loop
    A simple game loop is created to say how many frames we want per second and later how many updates per second we want. If a set duration has passed, it is time for a new frame and we need another refresh of the screen. In this case a repaint.
    //repaint();

--- src/main/GamePanel.java
+++ src/main/GamePanel.java
@@ -6,12 +6,21 @@ import inputs.Mouseinputs;
 import java.awt.*;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
+import java.util.Random;
 import javax.swing.JPanel;
 
 public class GamePanel extends JPanel {
     private Mouseinputs mouseInputs;
-    private int xDelta = 100, yDelta = 100;
+    private float xDelta = 100, yDelta = 100;
+    private float xDir=1f, yDir=1f;
+    private int frames =0;
+    private long lastCheck =0;
+    private Color color= new Color(55,77,99);
+    private Random random;
+
     public GamePanel() {
+
+        random=new Random();
         mouseInputs = new Mouseinputs(this);
         addKeyListener(new Keyboardinputs(this));
         addMouseListener(mouseInputs);
@@ -20,24 +29,45 @@ public class GamePanel extends JPanel {
 
     public void changeXDelta(int value) {
         this.xDelta += value;
-        repaint();
+
     }
 
     public void changeYDelta(int value) {
         this.yDelta += value;
-        repaint();
+
     }
 
     public void setRectPos(int x, int y) {
         this.xDelta = x;
         this.yDelta = y;
-        repaint();
+
     }
 
     public void paintComponent(Graphics g) {
         super.paintComponent(g);
+        updateRectangle();
 
-        g.fillRect(xDelta,yDelta,50,100);
+        g.setColor(color);
+        g.fillRect((int)xDelta,(int)yDelta,50,100);
 
     }
+    private void updateRectangle() {
+        xDelta += xDir;
+        if ( xDelta>400 || xDelta<0) {
+            xDir *= -1;
+            color = getRndColor();
+        }
+        yDelta += yDir;
+        if (yDelta>400 || yDelta<0)
+            yDir*=-1;
+        color= getRndColor();
+
+    }
+    private Color getRndColor() {
+        int r= random.nextInt(255);
+        int g=random.nextInt(255);
+        int b=random.nextInt(255);
+        return new Color(r,g,b);
 }
+}
+
