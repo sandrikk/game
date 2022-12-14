2130f701d9c08752cf8d09a216701a40ac9a5aaa
MiguelKTR <110743931+MiguelKTR@users.noreply.github.com>
2022-12-14 18:16:45 +0100

    Update Player.java

--- src/entities/Player.java
+++ src/entities/Player.java
@@ -5,32 +5,41 @@ import java.awt.image.BufferedImage;
 import java.io.IOException;
 import java.io.InputStream;
 
-public class Player extends Entity{
+public class Player extends Entity {
     private BufferedImage[][] animations;
+
     public Player(float x, float y) {
         super(x, y);
     }
+
     public void update() {
 
     }
+
     public void render() {
 
     }
-    private void loadAnimations() {
+
+    private void loadAnimations() throws IOException {
         InputStream istream = getClass().getResourceAsStream("/characters8.png");
         try {
-          BufferedImage  img = ImageIO.read(istream);
-            animations = new BufferedImage[5][5];
+            BufferedImage img = ImageIO.read(istream);
 
-            for (int j = 0; j < animations.length; j++) {
-                for (int i = 0; i < animations[j].length; i++) {
+            animations = new BufferedImage[5][5];
+            for (int j = 0; j < animations.length; j++)
+                for (int i = 0; i < animations[j].length; i++)
                     animations[j][i] = img.getSubimage(i * 64, j * 40, 64, 40);
         } catch (IOException e) {
             e.printStackTrace();
-        }
-
+        } finally {
+            try {
+                istream.close();
+            } catch (IOException e) {
+                e.printStackTrace();
             }
 
         }
+
     }
 }
+
