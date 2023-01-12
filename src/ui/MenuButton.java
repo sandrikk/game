8908323870904bf50526b388671ad2095c91790d
Sandrikk <krevovasandra@gmail.com>
2023-01-12 13:25:08 +0100

    add images and class

--- src/ui/MenuButton.java
+++ src/ui/MenuButton.java
@@ -1,7 +1,24 @@
 package ui;
 
-public class MenuButton {
-    public MenuButton(int xPos, int yPos, int rowIndex) {
+import gamestates.Gamestate;
 
+import java.awt.image.BufferedImage;
+
+public class MenuButton {
+    private int xPos, yPos, rowIndex;
+    private Gamestate state;
+    private BufferedImage[] images;
+    public MenuButton(int xPos, int yPos, int rowIndex, Gamestate state) {
+        this.xPos = xPos;
+        this.yPos = yPos;
+        this.state = state;
+        this.rowIndex = rowIndex;
+        loadImages();
     }
+
+    private void loadImages() {
+    }
+
 }
+
+
