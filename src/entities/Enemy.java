358dccc3895d96dea1f504bf42c3621894c93d26
georgistoychev003 <118820392+georgistoychev003@users.noreply.github.com>
2023-01-17 18:57:17 +0100

    Update Enemy.java

--- src/entities/Enemy.java
+++ src/entities/Enemy.java
@@ -1,14 +1,23 @@
 package entities;
+
+import main.Game;
+
 import static utilz.Constants.EnemyConstants.*;
+import static utilz.HelpMethods.*;
+
 
 public abstract class Enemy extends Entity {
     private int aniIndex, enemyState, enemyType;
     private int aniTick, aniSpeed = 25;
+    private boolean firstUpdate = true;
+    private boolean inAir;
+    private float fallSpeed;
+    private float gravity = 0.04f * Game.scaling;
 
     public Enemy(float x, float y, int width, int height, int enemyType) {
         super(x, y, width, height);
         this.enemyType = enemyType;
-        initImagebox(x,y,width,height);
+        initImagebox(x, y, width, height);
     }
 
     private void updateAnimationTick() {
@@ -26,8 +35,23 @@ public abstract class Enemy extends Entity {
         updateMove(lvlData);
         updateAnimationTick();
     }
-    private void updateMove(int[][] lvlData) {
 
+    private void updateMove(int[][] lvlData) {
+        if (firstUpdate)
+            if (!IsEntityOnFloor(imagebox, lvlData))
+                inAir = true;
+
+        if (inAir) {
+            if (CanMoveHere(imagebox.x, imagebox.y + fallSpeed,imagebox.width, imagebox.height, lvlData)) {
+                imagebox.y += fallSpeed;
+                fallSpeed += gravity;
+            } else {
+                inAir = false;
+                imagebox.y = GetEntityYPosUnderRoofOrAboveFloor(imagebox, fallSpeed);
+            }
+        } else {
+
+        }
     }
 
     public int getAniIndex() {
